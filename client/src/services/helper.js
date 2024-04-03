d2886e473f912da933d3506fd020d72d8683094c
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2024-04-03 19:09:35 +0530

    ADD:vercel

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,3 +1,1 @@
-export const BASE_URL = "https://excelerateserverapi.vercel.app/";
-// export const BASE_URL = "https://mernappbackend.onrender.com"
-// https://excelerateclientapi.vercel.app/
+export const BASE_URL = "https://excelerateserverapi.vercel.app";
