9bfaa10a426a3980839dc521ffe2639d7b6f0cbf
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2024-04-03 18:42:44 +0530

    ADD:error

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://excelerateserverapi.vercel.app";
+export const BASE_URL = "https://excelerateserverapi.vercel.app/";
 // export const BASE_URL = "https://mernappbackend.onrender.com"
+// https://excelerateclientapi.vercel.app/
